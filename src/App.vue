2bae715cf7146f35c5426c118f5361537336f0c4
Killian <killian.boularand@outlook.fr>
2025-01-28 12:12:20 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+import './index.css'
+import Button from './stories/Button.vue'
+
+</script>
+
+<template>
+ <Button label="Start workout" primary size="large" />
+</template>
+
+<style scoped></style>
